408bd2bee51da1398580886d549041629313ee88
IshanThapa <ishsin.2020@gmail.com>
2024-01-15 18:36:54 +0530

    7pm150124

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from src.Drugactivity-EDA.pipelines.prediction_pipeline import CustomData
+# from src.Drugactivity-EDA.pipelines.prediction_pipeline import CustomData
 
-customdataobj = CustomData()
+# customdataobj = CustomData()
 
-#python test.py
+# #python test.py
 
+print("changes")
